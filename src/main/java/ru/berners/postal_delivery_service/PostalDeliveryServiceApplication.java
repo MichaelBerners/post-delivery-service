7e47856b0568242415d2a16db446fa81b5674463
mihail <mihail_belonogov@bk.ru>
2024-01-29 10:48:17 +0300

    first commit

---
+++ src/main/java/ru/berners/postal_delivery_service/PostalDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.berners.postal_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostalDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/berners/postal_delivery_service/PostalDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.berners.postal_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
