81ae03abb6880454bca119eb247f08d995603775
mihail <mihail_belonogov@bk.ru>
2024-01-29 11:37:24 +0300

    add entity Postage and PostageType

---
+++ src/main/java/ru/berners/postal_delivery_service/domain/entity/Postage.java
@@ -0,0 +1,8 @@
+package ru.berners.postal_delivery_service.domain.entity;
+
+public class Postage {
+
+    private Long id;
+    private String recipientName;
+
+}
---
+++ src/main/java/ru/berners/postal_delivery_service/domain/entity/PostageType.java
@@ -0,0 +1,10 @@
+package ru.berners.postal_delivery_service.domain.entity;
+
+/**
+ * Типы почтовых отправлений (письмо, посылка, бандероль, открытка)
+ */
+public enum PostageType {
+    LATTER, PACKAGE, PARCEL, POSTCARD
+
+
+}
